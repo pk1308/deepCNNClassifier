b46d9ba6f064d5f405afffc0ef1a0f2bc1d84f6e
Prince Kurien <princevkurien@gmail.com>
2022-09-21 14:55:00 +0400

    Test case added & git added

--- tests/unit/utils_test.py
+++ tests/unit/utils_test.py
@@ -8,10 +8,7 @@ from deepClassifier.utils import read_yaml
 
 
 class Test_read_yaml:
-    yaml_files = [
-        "tests/data/empty.yaml",
-        "tests/data/demo.yaml"
-    ]
+    yaml_files = ["tests/data/empty.yaml", "tests/data/demo.yaml"]
 
     def test_read_yaml_empty(self):
         with pytest.raises(ValueError):
